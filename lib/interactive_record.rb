fe4d5bde68e5847c1f1da54a4fe059a777cf5283
Jazmin M <jazminmerckel@gmail.com>
2019-12-09 04:06:51 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,32 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
-  
+
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+
+  def self.table_name
+    self.to_s.downcase.pluralize
+  end
+
+  def self.column_names
+    DB[:conn].results_as_hash = true
+
+    sql = "PRAGMA table_info('#{table_name}')"
+
+    table_info = DB[:conn].execute(sql)
+    column_names = []
+
+    table_info.each do |column|
+      column_names << column["name"]
+    end
+
+    column_names.compact
+
+  end
+
+
 end
